28866b8f936419708b237042db670b45741e3529
romkalan <romkalan@mail.ru>
2023-04-03 22:27:46 +0300

    add final in the name of class taskListViewController

--- TaskList/ViewControllers/TaskListViewController.swift
+++ TaskList/ViewControllers/TaskListViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class TaskListViewController: UITableViewController {
+final class TaskListViewController: UITableViewController {
     
     private let cellID = "task"
     private var taskList: [Task] = []
